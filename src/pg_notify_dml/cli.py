3c106480c36157a6c2c4c52a139752980fafe04d
Nahuel <D3f0@users.noreply.github.com>
2023-09-05 23:50:37 +0300

    :art: Use a dataclass for Click config
    Add pass_context to sub-commands

--- src/pg_notify_dml/cli.py
+++ src/pg_notify_dml/cli.py
@@ -1,10 +1,23 @@
+from functools import lru_cache
 import click
-from dataclasses import dataclass
-
+from dataclasses import dataclass, field
+from sqlalchemy import create_engine, Engine, MetaData
 
 @dataclass
 class Config:
     connection: str
+    engine: Engine = field(init=False, default=None)
+    metadata: MetaData = field(init=False, default=None, repr=False)
+
+    def __post_init__(self):
+        if self.connection:
+            self.engine = create_engine(self.connection)
+            if self.engine.driver not in {"psycopg", "psycopg2"}:
+                msg = "This tool only support Postgres databases"
+                raise ValueError(msg)
+            self.metadata = MetaData()
+            self.metadata.reflect(self.engine)
+
 
 pass_config = click.make_pass_decorator(Config)
 
@@ -18,6 +31,19 @@ def group(ctx: click.Context, connection: str):
 
 
 @group.command()
-def setup_triggers():
+@pass_config
+def show_tables(config: Config):
+    """Shows available tables in the connection"""
+    if config.metadata:
+        print(config.metadata.tables.keys(), sep="\n")
+    else:
+        print("A connection is required.")  # noqa: T201
+
+@group.command()
+@pass_config
+def setup_triggers(config: Config):
     """Setups the triggers"""
-    ...
+    if config.metadata:
+        print(config.metadata.tables.keys())
+    else:
+        print("A connection is required.")  # noqa: T201
--- src/pg_notify_dml/triggers.py
+++ src/pg_notify_dml/triggers.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 import logging
-
+from typing import Dict
 
 TRIGGER = """
 CREATE TRIGGER {table}_notify_event
@@ -8,7 +8,13 @@ AFTER INSERT OR UPDATE OR DELETE ON {table}
     FOR EACH ROW EXECUTE PROCEDURE notify_event();
 """
 
-def get_tables(meta: sqlalchemy.MetaData):
+
+
+def get_tables(meta: sqlalchemy.MetaData) -> Dict[str, str]:
     for name, table in meta.tables.items():
         sql = TRIGGER.format(table=name)
         logging.debug("SQL for %s: %s", table, sql)
+
+def ensure_event_function(engine: sqlalchemy.Engine):
+    """Ensures the event function is present in the engine"""
+    ...
